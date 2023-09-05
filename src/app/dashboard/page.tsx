d8897c89da667fc218600307a8e06c55661b4cac
Emmanueluko3 <Emmanueluko90@gmail.com>
2023-09-05 22:18:48 +0000

    Fixes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,39 +1,15 @@
-import Card from "@/components/cards/card";
-import AreaChart from "@/components/charts/areaChart";
 import Navbar from "@/components/navbar/navbar";
 import ScheduledPayment from "@/components/scheduledPayment/scheduledPayment";
 import Sidebar from "@/components/sidebar/sidebar";
 import Transactions from "@/components/transactions/transactions";
 import dynamic from "next/dynamic";
+import Finance from "@/components/finance/finance";
 
 const DynamicComponentWithNoSSR = dynamic(
   () => import("@/components/charts/areaChart"),
   { ssr: false }
 );
 export default function Dashboard() {
-  const financialCategories = [
-    {
-      title: "Balance",
-      price: 200000,
-      duration: "Last Month",
-      percentage: 5,
-      variant: "#0784C7",
-    },
-    {
-      title: "Expenses",
-      price: 200000,
-      duration: "Last Month",
-      percentage: 10,
-      variant: "#3D315B",
-    },
-    {
-      title: "Savings",
-      price: 200000,
-      duration: "Last Month",
-      percentage: 5,
-      variant: "#F75C03",
-    },
-  ];
   return (
     <div className="flex lg:pl-8 lg:px-0 bg-[#fff]">
       <div>
@@ -42,17 +18,8 @@ export default function Dashboard() {
       <div className="w-full">
         <Navbar />
         <div className="bg-[#F9FAFA] h-full p-5 pt-20 pb-24 lg:pb-5 lg:pt-5 w-full lg:pr-9">
-          <div className="grid grid-flow-row lg:grid-cols-3 gap-3 mb-5">
-            {financialCategories.map((item, index) => (
-              <Card
-                key={index}
-                title={item.title}
-                price={item.price}
-                duration={item.duration}
-                percentage={item.percentage}
-                variant={item.variant}
-              />
-            ))}
+          <div className="mb-5">
+            <Finance />
           </div>
 
           <div className="w-full mb-5 grid grid-flow-row lg:grid-cols-3 gap-3">
---
+++ src/components/finance/finance.tsx
@@ -0,0 +1,75 @@
+"use client";
+import * as React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
+import Card from "../cards/card";
+
+const Finance: React.FC = () => {
+  const financialCategories = [
+    {
+      title: "Balance",
+      price: 200000,
+      duration: "Last Month",
+      percentage: 5,
+      variant: "#0784C7",
+    },
+    {
+      title: "Expenses",
+      price: 200000,
+      duration: "Last Month",
+      percentage: 10,
+      variant: "#3D315B",
+    },
+    {
+      title: "Savings",
+      price: 200000,
+      duration: "Last Month",
+      percentage: 5,
+      variant: "#F75C03",
+    },
+  ];
+  return (
+    <>
+      <div className="lg:grid grid-flow-row lg:grid-cols-3 gap-3 hidden">
+        {financialCategories.map((item, index) => (
+          <Card
+            key={index}
+            title={item.title}
+            price={item.price}
+            duration={item.duration}
+            percentage={item.percentage}
+            variant={item.variant}
+          />
+        ))}
+      </div>
+
+      <div className="lg:hidden">
+        <Swiper
+          spaceBetween={20}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+          {financialCategories.map((item, index) => (
+            <SwiperSlide>
+              <Card
+                key={index}
+                title={item.title}
+                price={item.price}
+                duration={item.duration}
+                percentage={item.percentage}
+                variant={item.variant}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </>
+  );
+};
+
+export default Finance;
