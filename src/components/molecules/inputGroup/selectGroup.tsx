02b84a3b1be14d1c739d9ccdbfc2663c0582922a
Emmanueluko3 <Emmanueluko90@gmail.com>
2023-09-29 18:10:31 +0000

    Fixes

--- src/components/molecules/inputGroup/selectGroup.tsx
+++ src/components/molecules/inputGroup/selectGroup.tsx
@@ -15,19 +15,21 @@ const SelectGroup: React.FC<SelectGroupProps> = ({
   return (
     <div>
       <p className="text-base text-gray-950 mb-1">{label}</p>
-      <select
-        {...rest}
-        className="border w-full border-gray-300 text-customGray1 bg-[#F9F9FB] rounded-lg p-3 focus:outline-none focus:border-customBlue"
-      >
-        <option value="" disabled selected>
-          {placeholder}
-        </option>
-        {options.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.label}
+      <div className="border w-full border-gray-300 text-customGray1 bg-[#F9F9FB] rounded-lg px-3 py-[5px]  focus:border-customBlue">
+        <select
+          {...rest}
+          className="w-full focus:border-none focus:outline-none bg-transparent"
+        >
+          <option value="" disabled selected>
+            {placeholder}
           </option>
-        ))}
-      </select>
+          {options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+      </div>
     </div>
   );
 };
